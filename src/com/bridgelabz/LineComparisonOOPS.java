bf65352fcb73bddfcec5efe61933316356902335
Siraj <sirajkhanc2305@gmail.com>
2021-08-26 14:05:58 +0530

    LineComparison

--- src/com/bridgelabz/LineComparisonOOPS.java
+++ src/com/bridgelabz/LineComparisonOOPS.java
@@ -38,23 +38,28 @@ public class LineComparisonOOPS {
         System.out.print("Enter y2 of line 2:");
         int q2 = scaq.nextInt();
 
-        LineComparisonOOPS obj = new LineComparisonOOPS();
-        double length1 = obj.calculate(x1, x2, y1, y2);
-        double length2 = obj.calculate(p1, p2, q1, q2);
-        obj.compare(length1, length2);
+        LineComparisonOOPS obj=new LineComparisonOOPS();
+        double length1=obj.calculate(x1,x2,y1,y2);
+        double length2=obj.calculate(p1,p2,q1,q2);
+        obj.compare(length1,length2);
     }
 
     public double calculate(int x1, int x2, int y1, int y2) {
         int a = x2 - x1;
         int b = y2 - y1;
         double length = Math.sqrt((a * a) + (b * b));
-        System.out.println("Distance between the given coordinates are : " + length);
+        System.out.println("Distance between the given coordinates are : "+length);
         return length;
     }
 
     public void compare(double length1, double length2) {
-        if (length1 == length2) {
+        if(length1 > length2) {
+            System.out.println("1st line is greater");
+        }else if(length1==length2) {
             System.out.println("Both lines are equal");
+        }else {
+            System.out.println("2nd line is greater");
         }
     }
+
 }
